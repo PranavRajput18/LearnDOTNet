cd6afab83da8f972a31dd631f6bcc52b335c318b
pranav <pranav.rajput@nonstopio.com>
2025-05-15 12:03:18 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] student = new string[] { "Pranav","Jhone","Bob"};
+            return Ok(student);
+        }        
+    }
+}
