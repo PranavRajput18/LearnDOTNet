682ad9000b541047961954f75d9a2f97b6745448
pranav <pranav.rajput@nonstopio.com>
2025-05-16 19:47:25 +0530

    feat: add validation attributes to AddRegionRequestDto properties

--- NZWalks.API/Models/DTO/AddRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/AddRegionRequestDto.cs
@@ -1,8 +1,16 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class AddRegionRequestDto
     {
+        [Required]
+        [MaxLength(3,ErrorMessage = "Code must be 3 characters")]
+        [MinLength(3,ErrorMessage = "Code must be 3 Characters")]
         public string Code { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
     }
