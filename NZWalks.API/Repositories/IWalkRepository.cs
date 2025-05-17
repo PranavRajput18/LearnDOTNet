5607c12eacb60ce0df06b36813442df7004d2f23
pranav <pranav.rajput@nonstopio.com>
2025-05-17 23:45:20 +0530

    feat: update GetAll method in IWalkRepository to include filtering and pagination parameters

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Repositories
 {
     public interface IWalkRepository
     {
-        Task<List<Walk>> GetAll();
+        Task<List<Walk>> GetAllAsync(string? filterOn ,string? filterQuery,string? sortBy,bool asending,int pageSize,int pageNumber);
         Task<Walk> CreateAsync(Walk walk);
         Task<Walk> GetByIdAsync(Guid id);
         Task<Walk> UpdateAsync(Walk walk,Guid id);
